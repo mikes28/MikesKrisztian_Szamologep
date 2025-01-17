616a6e7089140022904ca1f6ae91f878109b2bba
mikes28 <mikeskrisztian06@gmail.com>
2025-01-17 11:41:17 +0100

    Osszeadas kesz

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    System.Console.WriteLine(Osszeadas(1000,2000));
+    }
+    private static Int32 Osszeadas(Int32 numA, Int32 numB){
+        return numA+numB;
+    }
+}
