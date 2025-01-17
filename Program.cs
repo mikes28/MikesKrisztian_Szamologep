af26c84625a0d8189155c974d92b47e131a9a8f5
mikes28 <mikeskrisztian06@gmail.com>
2025-01-17 11:53:52 +0100

    Szazalek kesz

--- Program.cs
+++ Program.cs
@@ -12,13 +12,19 @@
     private static Int32 Kivonas(Int32 numA, Int32 numB)
     {
         return numA - numB;
-    }    private static Int32 Osztas(Int32 numA, Int32 numB)
+    }
+    private static Int32 Osztas(Int32 numA, Int32 numB)
     {
         return numA / numB;
     }
-        private static Int32 Szorzas(Int32 numA, Int32 numB)
+    private static Int32 Szorzas(Int32 numA, Int32 numB)
     {
         return numA * numB;
+    }
+
+        private static Int32 Szazalek(Int32 numA, Int32 numB)
+    {
+        return numA % numB;
     }
 
 }
